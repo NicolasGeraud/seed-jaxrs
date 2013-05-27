4b63e44d66239fac280362ba80152bf0aa904fbf
Nicolas Géraud <nicolas.geraud@gmail.com>
2013-05-27 11:44:20 +0200

    use Response.Status code instead of integer

--- src/main/java/fr/client/project/resources/exceptions/NotFoundException.java
+++ src/main/java/fr/client/project/resources/exceptions/NotFoundException.java
@@ -2,12 +2,13 @@ package fr.client.project.resources.exceptions;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 public class NotFoundException extends WebApplicationException {
 
   public NotFoundException() {
     // The content length is kind of a hack but it forces Tomcat and others
     // to respond with empty body
-    super(Response.status(404).header("Content-Length", 0).build());
+    super(Response.status(NOT_FOUND).header("Content-Length", 0).build());
   }
 }
